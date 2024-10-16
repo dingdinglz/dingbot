5ec6bf9277a610a73c9d0941699ffb8d2bb51e84
dingdinglz <1149978856@qq.com>
2024-10-16 17:08:00 +0800

    🐞 fix(keyword): 使私聊可用

--- bot/privatemessage.go
+++ bot/privatemessage.go
@@ -1,10 +1,30 @@
 package bot
 
 import (
+	"strings"
+
 	"github.com/LagrangeDev/LagrangeGo/client"
 	"github.com/LagrangeDev/LagrangeGo/message"
+	"github.com/dingdinglz/dingbot/database"
 )
 
 func BotPrivateMessageEvent(client *client.QQClient, event *message.PrivateMessage) {
+	messageText := event.ToString()
 
+	{
+		// 关键词
+		keywords := database.KeyWordGetAll()
+		for _, i := range keywords {
+			if i.Precise == "true" {
+				if messageText == i.Key {
+					client.SendPrivateMessage(event.Sender.Uin, []message.IMessageElement{message.NewText(i.Text)})
+				}
+			} else {
+				if strings.Contains(messageText, i.Key) {
+					client.SendPrivateMessage(event.Sender.Uin, []message.IMessageElement{message.NewText(i.Text)})
+				}
+
+			}
+		}
+	}
 }
