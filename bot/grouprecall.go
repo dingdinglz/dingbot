c9bc37fbd97ce28465f1090ae209084c220f96df
dingdinglz <1149978856@qq.com>
2024-11-26 16:58:47 +0800

    ✨ feat(event): 撤回事件

--- bot/grouprecall.go
+++ bot/grouprecall.go
@@ -1,9 +1,21 @@
 package bot
 
 import (
+	"strconv"
+
 	"github.com/LagrangeDev/LagrangeGo/client"
 	"github.com/LagrangeDev/LagrangeGo/client/event"
+	"github.com/dingdinglz/dingbot/database"
+	"github.com/robertkrimen/otto"
 )
 
 func GroupRecallEvent(client *client.QQClient, event *event.GroupRecall) {
+	messageText := database.MessageGetText("group", uint32(event.Sequence))
+	RunPlugin(func(vm *otto.Otto) {
+		vm.Set("a", messageText)
+		vm.Set("b", strconv.Itoa(int(event.GroupUin)))
+		vm.Set("c", strconv.Itoa(int(event.AuthorUin)))
+		vm.Set("d", strconv.Itoa(int(event.OperatorUin)))
+		vm.Run("GroupRecallEvent(a,b,c,d)")
+	})
 }
